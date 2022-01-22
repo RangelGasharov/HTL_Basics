cf2b77c311a8900cda4b488268cdda3f978ee7d4
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-01-22 15:43:21 +0100

    Magische Formel Aufgabe #2 (Final)

--- 02_Javascript/08_magicformel/script.js
+++ 02_Javascript/08_magicformel/script.js
@@ -1,18 +1,64 @@
 function generatebmi() {
 
-    let value1 = document.getElementById("Größe").value;
-    let value2 = document.getElementById("Gewicht").value;
+    let value1 = document.getElementById("size").value;
+    let value2 = document.getElementById("weight").value;
     let result = (value1 / value2) - 3.4;
-    console.log(result);
-    sort()
+    Math.floor(result, -1)
+    console.log(result)
+    printvalues(result)
 }
 
-function sort() {
-    if (result < 0) {
-        console.log("Neagativ");
-    } else if (result > 0) {
-        console.log("Positiv)");
+function printvalues(result) {
+
+    if (result > 0) {
+        let j = "";
+        for (let i = 0; i < result; i++) {
+            j += i + "<br>";
+            document.getElementById("output").innerHTML = "Positiv" + " " +
+                j + " (" + result + ") ";
+            console.log(i)
+        }
+    } else if (result < 0) {
+        let j = "";
+        for (i = 0; i > result; i--) {
+            j += i + "<br>";
+            document.getElementById("output").innerHTML = "Negativ" + " " +
+                j + " (" + result + ") ";
+            console.log(i)
+        }
     } else {
-        console.log("NULL");
+        document.getElementById("output").innerHTML = "NULL" + " " + 0;
     }
+
+
+
+
 }
+
+
+/* https://www.w3schools.com/js/tryit.asp?filename=tryjs_loop_for_ex
+function printvalues2(result) {
+
+    if (result < 0) {
+
+        for (abstand = result; abstand <= 0; abstand++) {
+            console.log(abstand);
+
+        }
+        document.getElementById("Ausgabewert").innerHTML = "Negativ" + " " + abstand;
+    } else if (result > 0) {
+
+        for (abstand = result; abstand >= 0; abstand--) {
+            console.log(abstand);
+
+        }
+        document.getElementById("Ausgabewert").innerHTML = "Positiv" + " " + abstand;
+    } else {
+
+        for (abstand = result; abstand = 0; abstand) {
+            console.log(abstand);
+        }
+        document.getElementById("Ausgabewert").innerHTML = "NULL" + " " + abstand;
+    }
+
+} */

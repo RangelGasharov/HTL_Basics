d745f08e3724b36b432b8ce227c15914f43515f8
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-02-03 18:36:12 +0100

    Testbeispiel1 #2, Verbesserung von Zuhause

--- Tests/script.js
+++ Tests/script.js
@@ -2,35 +2,40 @@ document.getElementById("generatebutton").addEventListener("click", function() {
     calculate();
 });
 
-
-
 function calculate() {
     let number = document.getElementById("inputfield").value;
     let numberRounded = Math.round(number);
-    execute(numberRounded);
+    execute(numberRounded)
 }
 
 function execute(numberRounded) {
     if (numberRounded > 0) {
         let j = ""
-        for (i = 0; i < numberRounded; i++) {
+        for (i = 0; i <= numberRounded; i++) {
             if (i == 0) {
                 seperator = ""
             } else {
                 seperator = ", "
             }
-            j += i + seperator;
+            j += seperator + i;
+            document.getElementById("outputs").innerHTML = j;
         }
 
     } else if (numberRounded < 0) {
         let j = ""
-        for (i = 0; i > numberRounded; i--) {
+        for (i = 0; i >= numberRounded; i--) {
             if (i == 0) {
                 seperator = ""
             } else {
                 seperator = ", "
             }
-            j += i + seperator;
+            j += seperator + i;
+
+            document.getElementById("outputs").innerHTML = j;
+        } {
+
         }
+    } else {
+        document.getElementById("outputs").innerHTML = 0;
     }
 }

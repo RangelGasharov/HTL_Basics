5627c99b6fdc5172d2636d0546742eb0b2476a1d
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-02-25 20:40:29 +0100

    Snake #1

--- 04_Games/01.2_Snake_JS/script.js
+++ 04_Games/01.2_Snake_JS/script.js
@@ -1,13 +1,38 @@
 let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d')
+let ctx = canvas.getContext('2d');
+let rows = 20;
+let cols = 30;
+let snake = [{ x: 29, y: 0 }];
+let food = { x: 4, y: 5 };
+canvas.widht = 720;
+canvas.height = 480;
+let cellWidht = canvas.widht / cols;
+let cellHeight = canvas.height / rows;
+let direction = 'LEFT';
 
-draw()
+setInterval(gameLoop, 500);
+
+draw();
 
 function draw() {
     ctx.fillStyle = 'black';
-    ctx.fillRect(10, 20, 20, 20)
+    ctx.fillRect(0, 0, canvas.widht, canvas.height)
+    ctx.fillStyle = 'red';
+
+    snake.forEach(part => add(part.x, part.y));
+
+    ctx.fillStyle = 'yellow';
+    add(food.x, food.y);
+
+    requestAnimationFrame(draw);
+}
+
+function add(x, y) {
+    ctx.fillRect(x * cellHeight, y * cellHeight, cellWidht - 1, cellHeight - 1)
 }
 
 function gameLoop() {
-
+    snake[0].x--;
 }
+
+function keyDown(e)

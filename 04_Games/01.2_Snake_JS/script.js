6b56911a6eddcd1501f37d94d7028096a5d2b1af
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-02-25 19:41:45 +0100

    Javascript Games

---
+++ 04_Games/01.2_Snake_JS/script.js
@@ -0,0 +1,13 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d')
+
+draw()
+
+function draw() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(10, 20, 20, 20)
+}
+
+function gameLoop() {
+
+}

8382244f46a1cf2d2767ec89d04fef61035f4dc6
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-03-08 20:47:47 +0100

    Calculator # 2

--- 02_Javascript/09_Taschenrechner/script.js
+++ 02_Javascript/09_Taschenrechner/script.js
@@ -2,5 +2,20 @@ function appendOperation(operation) {
     document.getElementById("resultArea").innerHTML += operation;
 }
 
+function calculateResult() {
+    let container = document.getElementById("resultArea");
+    let result = eval(container.innerHTML);
+    container.innerHTML = result;
+}
+
+function deleteLast() {
+    let container = document.getElementById("resultArea");
+    if (container.innerHTML.endsWith(' ')) {
+        container.innerHTML = container.innerHTML.slice(0, -3)
+    } else {
+        container.innerHTML = container.innerHTML.slice(0, -1)
+    }
+
+}
 
 //https: //www.youtube.com/watch?v=58gAY0cbgek

a9f0ca821db96da6591548abe5e155fce0957ad0
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-03-19 17:22:47 +0100

    Experiment While-Loop #3

--- 02_Javascript/10_Experiment/script.js
+++ 02_Javascript/10_Experiment/script.js
@@ -4,7 +4,7 @@ document.getElementById("startProcess1").addEventListener("click", function() {
 });
 
 document.getElementById("startProcess2").addEventListener("click", function() {
-    calculateNumber2()
+    calculateNumber1()
 });
 
 let pointreached1 = false;
@@ -15,7 +15,6 @@ function calculateNumber1() {
     if (value1 == '') {
         alert('Please, specify the starting value!')
     }
-    let processnumber = 0;
     let pointtoreach1 = document.getElementById("pointtoreach1").value;
     if (pointtoreach1 == '') {
         alert('Please, specify the ending value!')
@@ -24,27 +23,30 @@ function calculateNumber1() {
     if (slope1 == '') {
         alert('Please, specify the value of the slope!')
     }
-
     let propabilitypoint1 = document.getElementById("propabilitypoint1").value;
     if (propabilitypoint1 == '') {
         alert('Please, specify range of propability!')
     }
+    let processnumber = 0;
 
     while (!pointreached1) {
         let propability = Math.random() * 100;
-        console.log("Current value1: " + value1)
+        console.log("Current value: " + value1)
         console.log("Number of rounds: " + processnumber)
         console.log("Propability-rate: " + propability)
 
-        if (propability < propabilitypoint1 && value1 > pointtoreach1) {
+        if (value1 > pointtoreach1 && propability < propabilitypoint1) {
             value1 = value1 * (1 - (slope1 / 100))
             processnumber = processnumber + 1;
-        } else if (propability > propabilitypoint1 && value1 > pointtoreach1) {
+        } else if (value1 > pointtoreach1 && propability > propabilitypoint1) {
             value1 = value1 * (1 + (slope1 / 100))
+            processnumber = processnumber + 1;
         } else if (value1 <= pointtoreach1) {
             pointreached1 = true;
             console.log("The endpoint was reached!")
         }
-        document.getElementById("outputs1").innerHTML = processnumber;
+
     }
+    document.getElementById('outputs1').innerHTML += processnumber + ' ';
+
 }

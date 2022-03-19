220d1f8cd4d2a8146c5bdbb7a43db2e27404e228
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-03-19 12:01:44 +0100

    Experiment While-Loops #2

--- 02_Javascript/10_Experiment/script.js
+++ 02_Javascript/10_Experiment/script.js
@@ -1,23 +1,44 @@
-let pitch = 0.10;
-let value = 100;
-let processnumber = 0;
-let pointtoreach = 0.9;
-let pointreached = false;
+document.getElementById("startProcess1").addEventListener("click", function() {
+    calculateNumber1()
+});
+
+document.getElementById("startProcess2").addEventListener("click", function() {
+    calculateNumber2()
+});
+
+let pointreached1 = false;
 
 
-while (!pointreached) {
-    let propability = Math.random() * 100;
-    console.log("Current value: " + value)
-    console.log("Number of rounds: " + processnumber)
-    console.log("Propability-rate: " + propability)
+function calculateNumber1() {
+    let value1 = document.getElementById("value1").value;
+    if (value1 == '') {
+        alert('Please, specify the starting value!')
+    }
+    let processnumber = 0;
+    let pointtoreach1 = document.getElementById("pointtoreach1").value;
+    if (pointtoreach1 == '') {
+        alert('Please, specify the ending value!')
+    }
+    let slope1 = document.getElementById("slope1").value;
+    if (slope1 == '') {
+        alert('Please, specify the value of the slope!')
+    }
 
-    if (propability < 60 && value > pointtoreach) {
-        value = value * (1 - pitch)
-        processnumber = processnumber + 1;
-    } else if (propability > 60 && value > pointtoreach) {
-        value = value * (1 + pitch)
-    } else if (value <= pointtoreach) {
-        pointreached = true;
-        console.log("The endpoint was reached!")
+    while (!pointreached1) {
+        let propability = Math.random() * 100;
+        console.log("Current value1: " + value1)
+        console.log("Number of rounds: " + processnumber)
+        console.log("Propability-rate: " + propability)
+
+        if (propability < 55 && value1 > pointtoreach1) {
+            value1 = value1 * (1 - (slope1 / 100))
+            processnumber = processnumber + 1;
+        } else if (propability > 55 && value1 > pointtoreach1) {
+            value1 = value1 * (1 + (slope1 / 100))
+        } else if (value1 <= pointtoreach1) {
+            pointreached1 = true;
+            console.log("The endpoint was reached!")
+        }
+
     }
 }

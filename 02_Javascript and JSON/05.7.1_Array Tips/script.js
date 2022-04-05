6648fdb3226068aa53e68bd349dc6db0371431a0
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-04-05 20:24:57 +0200

    Array Tips Part 2

--- 02_Javascript and JSON/05.7.1_Array Tips/script.js
+++ 02_Javascript and JSON/05.7.1_Array Tips/script.js
@@ -47,9 +47,11 @@ const horse = {
     age: 7
 }
 
+console.table(horse)
+
 const { name, size, skills } = horse;
 
-bio = `${name} is a ${size} skilled in ${skills.join(' & ')}`
+const bio = `${name} is a ${size} skilled in ${skills.join(' & ')}`
 
 console.log(bio);
 
@@ -63,6 +65,59 @@ console.log(bio2)
 
 
 console.log("--------------------------------------------------")
+
+const pikachu = { name: 'Pikachu' };
+const stats = { hp: 40, attack: 60, defense: 45 }
+
+const lvl0 = {...pikachu, ...stats }
+const lvl1 = {...pikachu, ...stats, hp: 45 }
+
+console.log(lvl0)
+console.log(lvl1)
+
+console.table(pikachu)
+
+let pokemon = ['Arbok', 'Raichu', 'Sandshrew'];
+
+pokemon = [...pokemon, 'Bulbasaur', 'Metapod', 'Weedle']
+
+console.table(pokemon)
+
+console.log("--------------------------------------------------")
+
+const orders = [500, 30, 99, 15, 223];
+
+console.table(orders)
+
+const total = orders.reduce((acc, cur) => acc + cur)
+
+const withTax = orders.map(v => v * 1.1)
+
+const highValue = orders.filter(v => v > 100)
+
+
+console.table({ total, withTax, highValue })
+
+console.log("-------------------------------------------------")
+
+const random = () => {
+    return Promise.resolve(Math.random())
+
+}
+
+const sumRandomAsyncNums = async() => {
+
+    const first = await random();
+    const second = await random();
+    const third = await random();
+
+    console.log(`Result ${first + second + third}`)
+
+}
+
+
+
+
 
 //https://www.youtube.com/watch?v=Mus_vwhTCq0
 //6:08

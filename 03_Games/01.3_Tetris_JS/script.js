65ac4e4657511a28dcb989c9cfa0d30fc008272e
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-04-16 19:35:54 +0200

    Tetris with JS #2

--- 03_Games/01.3_Tetris_JS/script.js
+++ 03_Games/01.3_Tetris_JS/script.js
@@ -1,29 +1,14 @@
 const cvs = document.getElementById('tetris');
+const ctx = cvs.getContext('2d');
+const scoreElement = document.getElementById("score");
 /*cvs.widht = 200;
 cvs.height = 400;*/
-const ctx = cvs.getContext('2d');
+
 const ROW = 20;
-const COLUMN = 10;
+const COL = 10;
+const SQ = SQUARESIZE = 30;
 const VACANT = "white" //Vacant Square Color
-const SQ = SQUARESIZE = 20
 
-
-let board = [];
-for (r = 0; r < ROW; r++) {
-    board[r] = [];
-    for (c = 0; c < COLUMN; c++) {
-        board[r][c] = VACANT;
-    }
-}
-
-function drawBoard() {
-    for (r = 0; r < ROW; r++) {
-        for (c = 0; c < COLUMN; c++) {
-            drawSquare(c, r, board[r][c]);
-        }
-    }
-}
-drawBoard()
 
 function drawSquare(x, y, color) {
     ctx.fillStyle = color;
@@ -31,6 +16,24 @@ function drawSquare(x, y, color) {
     ctx.strokeStyle = "black";
     ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
 }
+
+let board = [];
+for (r = 0; r < ROW; r++) {
+    board[r] = [];
+    for (c = 0; c < COL; c++) {
+        board[r][c] = VACANT;
+    }
+}
+
+function drawBoard() {
+    for (r = 0; r < ROW; r++) {
+        for (c = 0; c < COL; c++) {
+            drawSquare(c, r, board[r][c]);
+        }
+    }
+}
+
+drawBoard();
 
 //drawSquare(5, 6, "green")
 //drawSquare(3, 2, "red")
@@ -58,6 +61,169 @@ const Z = [
     ]
 ]
 
+const S = [
+    [
+        [0, 1, 1],
+        [1, 1, 0],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 0, 1]
+    ],
+    [
+        [0, 0, 0],
+        [0, 1, 1],
+        [1, 1, 0]
+    ],
+    [
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+    ]
+]
+
+const J = [
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0]
+    ],
+    [
+        [1, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 1],
+        [0, 1, 0],
+        [0, 1, 0]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 1]
+    ]
+]
+
+const T = [
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+    ],
+    [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+    ],
+    [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 1, 0]
+    ]
+]
+
+const L = [
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [1, 0, 0]
+    ],
+    [
+        [1, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+    ],
+    [
+        [0, 0, 1],
+        [1, 1, 1],
+        [0, 0, 0]
+    ]
+]
+
+const I = [
+    [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0]
+    ]
+]
+
+const O = [
+    [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]
+    ]
+]
+
+const PIECES = [
+    [Z, "red"],
+    [S, "green"],
+    [T, "cyan"],
+    [O, "indigo"],
+    [I, "blue"],
+    [L, "purple"],
+    [J, "orange"]
+];
+
+function randomPiece() {
+    let randomN = Math.floor(Math.random() * PIECES.length);
+    return new Piece(PIECES[randomN][0], PIECES[randomN][1])
+}
+
+let p = randomPiece();
+
 /*let piece = Z[0];
 const pieceColor = "orange"
 
@@ -71,54 +237,33 @@ for (r = 0; r < piece.length; r++) {
 
 function Piece(tetromino, color) {
     this.tetromino = tetromino;
+    this.color = color;
+
     this.tetrominoN = 0;
     this.activeTetromino = this.tetromino[this.tetrominoN];
-    this.color = color;
+
     this.x = 3;
     this.y = -2;
+}
+
+
+Piece.prototype.fill = function(color) {
     for (r = 0; r < this.activeTetromino.length; r++) {
         for (c = 0; c < this.activeTetromino.length; c++) {
+            // we draw only occupied squares
             if (this.activeTetromino[r][c]) {
-                drawSquare(c + 3, r + 2, this.color)
+                drawSquare(this.x + c, this.y + r, color);
             }
         }
     }
 }
 
-let piece = new Piece(Z, "blue")
-
 Piece.prototype.draw = function() {
-    for (r = 0; r < this.activeTetromino.length; r++) {
-        for (c = 0; c < this.activeTetromino.length; c++) {
-            if (this - this.activeTetromino[r][c]) {
-                drawSquare(this.x + c, this.y + r, this.color)
-            }
-        }
-    }
+    this.fill(this.color);
 }
 
 Piece.prototype.unDraw = function() {
-    for (r = 0; r < this.activeTetromino.length; r++) {
-        for (c = 0; c < this.activeTetromino.length; c++) {
-            if (this - this.activeTetromino[r][c]) {
-                drawSquare(this.x + c, this.y + r, VACANT)
-            }
-        }
-    }
-}
-
-document.addEventListener("keydown", CONTROL);
-
-function CONTROL(event) {
-    if (event.keyCode == 37) {
-        piece.moveLeft();
-    } else if (event.keyCode == 38) {
-        piece.rotate();
-    } else if (event.keyCode == 39) {
-        piece.moveRight();
-    } else if (event.keyCode == 40) {
-        piece.moveDown();
-    }
+    this.fill(VACANT);
 }
 
 Piece.prototype.moveDown = function() {
@@ -127,8 +272,10 @@ Piece.prototype.moveDown = function() {
         this.y++;
         this.draw();
     } else {
-
+        this.lock();
+        p = randomPiece();
     }
+
 }
 
 Piece.prototype.moveLeft = function() {
@@ -136,8 +283,6 @@ Piece.prototype.moveLeft = function() {
         this.unDraw();
         this.x--;
         this.draw();
-    } else {
-
     }
 }
 
@@ -146,18 +291,78 @@ Piece.prototype.moveRight = function() {
         this.unDraw();
         this.x++;
         this.draw();
-    } else {
+    }
+}
+
+Piece.prototype.rotate = function() {
+    let nextPattern = this.tetromino[(this.tetrominoN + 1) % this.tetromino.length];
+    let kick = 0;
+
+    if (this.collision(0, 0, nextPattern)) {
+        if (this.x > COL / 2) {
+            kick = -1;
+        } else {
+            kick = 1;
+        }
+    }
+
+    if (!this.collision(kick, 0, nextPattern)) {
+        this.unDraw();
+        this.x += kick;
+        this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length
+        this.activeTetromino = this.tetromino[this.tetrominoN];
+        this.draw();
+    }
+}
+
+let score = 0;
+
+Piece.prototype.lock = function() {
+    for (r = 0; r < this.activeTetromino.length; r++) {
+        for (c = 0; c < this.activeTetromino.length; c++) {
+            if (!this.activeTetromino[r][c]) {
+                continue;
+            }
+            if (this.y + r < 0) {
+                alert("Game Over")
+                gameOver = true;
+                break;
+            }
+            board[this.y + r][this.x + c] = this.color;
+        }
+    }
+    for (r = 0; r < ROW; r++) {
+        let isRowFull = true;
+        for (c = 0; c < COL; c++) {
+            isRowFull = isRowFull && (board[r][c] != VACANT);
+        }
+        if (isRowFull) {
+            for (y = r; y > 1; y--) {
+                for (c = 0; c < COL; c++) {
+                    board[y][c] = board[y - 1][c];
+                }
+                board[8][10] = board[7][10];
+            }
+            for (c = 0; c < COL; c++) {
+                board[0][c] = VACANT;
+            }
+            score += 10
+        }
 
     }
+    drawBoard();
+    scoreElement.innerHTML = score;
 }
 
 Piece.prototype.collision = function(x, y, piece) {
     for (r = 0; r < piece.length; r++) {
         for (c = 0; c < piece.length; c++) {
             if (!piece[r][c]) { continue; }
+
             let newX = this.x + c + x;
             let newY = this.y + r + y;
-            if (newX < 0 || newX >= COL || newY > ROW) {
+
+            if (newX < 0 || newX >= COL || newY >= ROW) {
                 return true;
             }
             if (newY < 0) { continue; }
@@ -169,6 +374,34 @@ Piece.prototype.collision = function(x, y, piece) {
     return false;
 }
 
+document.addEventListener("keydown", CONTROL);
 
+function CONTROL(event) {
+    if (event.keyCode == 37) {
+        p.moveLeft();
+    } else if (event.keyCode == 38) {
+        p.rotate();
+    } else if (event.keyCode == 39) {
+        p.moveRight();
+    } else if (event.keyCode == 40) {
+        p.moveDown();
+    }
+}
 
+let dropStart = Date.now();
+let gameOver = false;
+
+function drop() {
+    let now = Date.now();
+    let delta = now - dropStart;
+    if (delta > 1000) {
+        p.moveDown();
+        dropStart = Date.now();
+    }
+    if (!gameOver) {
+        requestAnimationFrame(drop);
+    }
+}
+
+drop();
 //https://www.youtube.com/watch?v=HEsAr2Yt2do

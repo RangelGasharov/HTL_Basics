feeb9ed5dc37d7b0b60c3d1d07ab5ec5f253a173
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-05-04 19:53:41 +0200

    Bouncy Ball #2

--- 03_Games/01.4_Bouncy Ball/script.js
+++ 03_Games/01.4_Bouncy Ball/script.js
@@ -1,96 +1,139 @@
 const FPS = 60;
-let bs1 = 30;
-let bs2 = 20;
-let bx1, by1, bx2, by2;
-let xv1, yv1, xv2, yv2;
-let canvas, context;
 let wallHits = 0;
 
-canvas = document.getElementById("gameCanvas")
-context = canvas.getContext("2d")
+const canvas = document.getElementById("gameCanvas")
+const context = canvas.getContext("2d")
 
-setInterval(update, 1000 / FPS);
-
-bx1 = canvas.width / 2;
-by1 = canvas.height / 2;
-
-bx2 = canvas.width / 2;
-by2 = canvas.height / 2;
-
-//pps --> pixels per seconds
-xv1 = Math.floor(Math.random() * 151 + 100) / FPS;
-yv1 = Math.floor(Math.random() * 151 + 100) / FPS;
-
-xv2 = Math.floor(Math.random() * 251 + 150) / FPS;
-yv2 = Math.floor(Math.random() * 251 + 150) / FPS;
-
-
-if (Math.floor(Math.random() * 2)== 0) {
-    xv1 = -xv1;
-}
-if (Math.floor(Math.random() * 2)== 0) {
-    xv2 = -xv2;
-}
-
-if (Math.floor(Math.random() * 2)== 0) {
-    yv1 = -yv1;
-}
-if (Math.floor(Math.random() * 2)== 0) {
-    yv2 = -yv2;
-}
-
-function update() {
-    bx1 += xv1;
-    by1 += yv1;
-
-    bx2 += xv2;
-    by2 += yv2;
-    
-    if(bx1 - bs1/2 < 0 && xv1 < 0){
-        xv1 = -xv1;
-        wallHits += 1;
+class Ball {
+    constructor({ ballSize, ballPosition = { x: 0, y: 0 }, ballVelocity = { x: 0, y: 0 }, ballColor }) {
+        this.ballSize = ballSize
+        this.ballPosition = ballPosition
+        this.ballVelocity = ballVelocity
+        this.ballColor = ballColor
     }
-    if(bx2 - bs2/2 < 0 && xv2 < 0){
-        xv2 = -xv2;
-        wallHits += 1;
-    }
-    
-    if(bx1 + bs1/2 > canvas.width && xv1 > 0){
-        xv1 = -xv1;
-        wallHits += 1;
-    }
-    if(bx2 + bs2/2 > canvas.width && xv2 > 0){
-        xv2 = -xv2;
-        wallHits += 1;
-    }
-    
-    if(by1 - bs1/2 < 0 && yv1 < 0){
-        yv1 = -yv1;
-        wallHits += 1;
-    }
-    if(by2 - bs2/2 < 0 && yv2 < 0){
-        yv2 = -yv2;
-        wallHits += 1;
-    }
-    
-    if(by1 + bs1/2 > canvas.height && yv1 > 0){
-        yv1 = -yv1;
-        wallHits += 1;
-    }
-    if(by2 + bs2/2 > canvas.height && yv2 > 0){
-        yv2 = -yv2;
-        wallHits += 1;
+    randomDirection() {
+        if (Math.floor(Math.random() * 2) == 0) {
+            this.ballVelocity.x = - this.ballVelocity.x
+        }
+
+        if (Math.floor(Math.random() * 2) == 0) {
+            this.ballVelocity.y = - this.ballVelocity.y
+        }
     }
 
-    context.fillStyle = "black";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = "red";
-    context.fillRect(bx1 - bs1 / 2, by1 - bs1 / 2, bs1, bs1);
-    context.fillStyle = "yellow";
-    context.fillRect(bx2 - bs2/2, by2 - bs2/2, bs2, bs2);
 
-    document.getElementById("totalWallHits").innerHTML = "Total amount of wall hits: " +  wallHits;
+    collisionDetector() {
+        this.ballPosition.x += this.ballVelocity.x
+        this.ballPosition.y += this.ballVelocity.y
+
+        if (this.ballPosition.x - this.ballSize / 2 < 0 && this.ballVelocity.x < 0) {
+            this.ballVelocity.x = - this.ballVelocity.x
+            wallHits += 1;
+        }
+
+        if (this.ballPosition.x + this.ballSize / 2 > canvas.width && this.ballVelocity.x > 0) {
+            this.ballVelocity.x = - this.ballVelocity.x
+            wallHits += 1;
+        }
+
+        if (this.ballPosition.y - this.ballSize / 2 < 0 && this.ballVelocity.y < 0) {
+            this.ballVelocity.y = - this.ballVelocity.y
+            wallHits += 1;
+        }
+
+        if (this.ballPosition.y + this.ballSize / 2 > canvas.height && this.ballVelocity.y > 0) {
+            this.ballVelocity.y = - this.ballVelocity.y
+            wallHits += 1;
+        }
+
+        document.getElementById("totalWallHits").innerHTML = "Total amount of wall hits: " + wallHits;
+    }
+
+    draw() {
+        context.fillStyle = "black";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        context.fillStyle = ball_1.ballColor
+        context.fillRect(
+            ball_1.ballPosition.x - ball_1.ballSize / 2,
+            ball_1.ballPosition.y - ball_1.ballSize / 2,
+            ball_1.ballSize, ball_1.ballSize)
+
+        context.fillStyle = ball_2.ballColor
+        context.fillRect(
+            ball_2.ballPosition.x - ball_2.ballSize / 2,
+            ball_2.ballPosition.y - ball_2.ballSize / 2,
+            ball_2.ballSize, ball_2.ballSize)
+
+        context.fillStyle = ball_3.ballColor
+        context.fillRect(
+            ball_3.ballPosition.x - ball_3.ballSize / 2,
+            ball_3.ballPosition.y - ball_3.ballSize / 2,
+            ball_3.ballSize, ball_3.ballSize)
+    }
+
+    update() {
+        this.draw()
+        this.collisionDetector()
+    }
 }
 
+const ball_1 = new Ball({
+    ballSize: 30,
+    ballPosition: {
+        x: canvas.width / 2,
+        y: canvas.height / 2
+    },
+    ballVelocity: {
+        x: Math.floor(Math.random() * 251 + 100) / FPS,
+        y: Math.floor(Math.random() * 251 + 100) / FPS
+    },
+    ballColor: "green"
+})
+
+ball_1.randomDirection()
+
+const ball_2 = new Ball({
+    ballSize: 20,
+    ballPosition: {
+        x: canvas.width / 2,
+        y: canvas.height / 2
+    },
+    ballVelocity: {
+        x: Math.floor(Math.random() * 151 + 100) / FPS,
+        y: Math.floor(Math.random() * 151 + 100) / FPS
+    },
+    ballColor: "red"
+})
+
+ball_2.randomDirection()
+
+const ball_3 = new Ball({
+    ballSize: 35,
+    ballPosition: {
+        x: canvas.width / 2,
+        y: canvas.height / 2
+    },
+    ballVelocity: {
+        x: Math.floor(Math.random() * 251 + 100) / FPS,
+        y: Math.floor(Math.random() * 251 + 100) / FPS
+    },
+    ballColor: "yellow"
+})
+
+ball_3.randomDirection()
+
+console.log(ball_1)
+console.log(ball_2)
+console.log(ball_3)
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    ball_1.update()
+    ball_2.update()
+    ball_3.update()
+}
+
+animate()
 
 //https://www.youtube.com/watch?v=DeqE2yn3v6Q

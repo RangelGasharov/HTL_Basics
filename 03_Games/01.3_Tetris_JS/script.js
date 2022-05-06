0b16fc2acf1ad82c3975b4188c9ddaa7907ae2e7
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-05-06 21:42:33 +0200

    Tetris with JS #3.1

--- 03_Games/01.3_Tetris_JS/script.js
+++ 03_Games/01.3_Tetris_JS/script.js
@@ -323,7 +323,7 @@ Piece.prototype.lock = function() {
             if (!this.activeTetromino[r][c]) {
                 continue;
             }
-            if (this.y - 2 + r < 0) {
+            if (this.y + r < 0) {
                 alert("Game Over")
                 gameOver = true;
                 break;

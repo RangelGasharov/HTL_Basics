a1bf9ded9729b78f7929a29cf7c5fbd8b10a8046
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-05-07 16:40:06 +0200

    Randomiser #2

--- 02_Javascript and JSON/05.4.2_Eigenes Projekt/script.js
+++ 02_Javascript and JSON/05.4.2_Eigenes Projekt/script.js
@@ -17,8 +17,14 @@ function generate() {
     let number1 = document.getElementById("figure1").value;
     let number2 = document.getElementById("figure2").value;
 
+    if ((number1 == "") || (number2 == "")) {
+        alert("Please make sure, that everything is filled in!")
+        return
+    }
+
     let amountOfRandomNumbers = parseInt(number2) - parseInt(number1) + 1;
     console.log(number1 + ":" + number2);
+    if (amountOfRandomNumbers = "") return
 
     let randomNumber = Math.floor(Math.random() * amountOfRandomNumbers) + parseInt(number1);
     document.getElementById("randomNumber").innerHTML = randomNumber;
@@ -27,6 +33,7 @@ function generate() {
 
 
 function newElement() {
+
     var li = document.createElement("li");
     var inputValue = document.getElementById("firstword").value + " -> " + randomNumber.innerHTML + "  " + unit.value;
     var t = document.createTextNode(inputValue);
@@ -38,5 +45,4 @@ function newElement() {
 
 
 
-
-/*https: //www.w3schools.com/howto/howto_js_todolist.asp */
+//https://www.w3schools.com/howto/howto_js_todolist.asp

15904ee7e138582606edcc5b18ca3b9024ed86f2
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-07-30 18:03:31 +0200

    Budget Tracker #2

---
+++ 02_Javascript and JSON/16_Budget Tracker/js/script.js
@@ -0,0 +1,5 @@
+import BudgetTracker from "./BudgetTracker.js";
+
+new BudgetTracker("#app");
+
+//https://www.youtube.com/watch?v=OnML-5Mk70o
--- 02_Javascript and JSON/16_Budget Tracker/script.js
+++
@@ -1,1 +0,0 @@
-//https://www.youtube.com/watch?v=OnML-5Mk70o

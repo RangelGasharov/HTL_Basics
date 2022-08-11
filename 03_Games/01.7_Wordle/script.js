4a7ae0f1951cfda41fbd6417f4aee8d63ca650cd
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-08-11 11:54:59 +0200

    Wordle Clone #2

---
+++ 03_Games/01.7_Wordle/script.js
@@ -0,0 +1,146 @@
+/*let dictionary, targetWords = [];
+
+let showDictionary = function () {
+}
+
+let showTargetWords = function () {  
+}
+
+
+let myDictionaryRequest = new Request("./dictionary.json")
+
+fetch(myDictionaryRequest)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        dictionary = data;
+        showDictionary();
+    });
+
+
+let myTargetWordsRequest = new Request("./targetWords.json")
+
+fetch(myTargetWordsRequest)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        targetWords = data;
+        showTargetWords();
+    });
+*/
+
+const WORD_LENGTH = 5;
+const alertContainer = document.querySelector("[data-alert-container]")
+const guessGrid = document.querySelector("[data-guess-grid]")
+const offsetFromDate = new Date(2022, 0, 1)
+const msOffset = Date.now() - offsetFromDate
+const dayOffset = msOffset / 1000 / 60 / 60 / 24
+const targetWord = "ahead" /* targetWords[Math.floor(dayOffset)] */
+
+
+startInteraction()
+
+function startInteraction() {
+    document.addEventListener("click", handleMouseClick)
+    document.addEventListener("keydown", handleKeyPress)
+}
+
+function stopInteraction() {
+    document.removeEventListener("click", handleMouseClick)
+    document.removeEventListener("keydown", handleKeyPress)
+}
+
+function handleMouseClick(e) {
+    if (e.target.matches("[data-key]")) {
+        pressKey(e.target.dataset.key)
+        return
+    }
+
+    if (e.target.matches("[data-enter]")) {
+        submitGuess()
+        return
+    }
+
+    if (e.target.matches("[data-delete]")) {
+        deleteKey()
+        return
+    }
+}
+
+function handleKeyPress(e) {
+    if (e.key === "Enter") {
+        submitGuess()
+        return
+    }
+
+    if (e.key === "Backspace" || e.key === "Delete") {
+        deleteKey()
+        return
+    }
+
+    if (e.key.match(/^[a-z]$/)) {
+        pressKey(e.key)
+        return
+    }
+}
+
+function pressKey(key) {
+    const activeTiles = getActiveTiles()
+    if (activeTiles.length >= WORD_LENGTH) return
+    const nextTile = guessGrid.querySelector(":not([data-letter])")
+    nextTile.dataset.letter = key.toLowerCase()
+    nextTile.textContent = key
+    nextTile.dataset.state = "active"
+}
+
+function deleteKey() {
+    const activeTiles = getActiveTiles()
+    const lastTile = activeTiles[activeTiles.length - 1]
+    if (lastTile == null) return
+    lastTile.textContent = ""
+    delete lastTile.dataset.state
+    delete lastTile.dataset.letter
+}
+
+function submitGuess() {
+    const activeTiles = [...getActiveTiles()]
+    if (activeTiles.length !== WORD_LENGTH) {
+        showAlert("Not long enough!")
+        shakeTiles(activeTiles)
+        return
+    }
+}
+
+function getActiveTiles() {
+    return guessGrid.querySelectorAll('[data-state="active"]')
+}
+
+function showAlert(message, duration = 1000) {
+    const alert = document.createElement("div")
+    alert.textContent = message
+    alert.classList.add("alert")
+    alertContainer.prepend(alert)
+    if (duration == null) return
+
+    setTimeout(() => {
+        alert.classList.add("hide")
+        alert.addEventListener("transitionend", () => {
+            alert.remove()
+        })
+    }, duration)
+}
+
+function shakeTiles(tiles) {
+    tiles.forEach(tile => {
+        tile.classList.add("shake")
+        tile.addEventListener("animationend", () => {
+            tile.classList.remove("shake")
+        }, { once: true })
+    })
+}
+
+//https://www.youtube.com/watch?v=Wak7iN4JZzU&t=1364s
+//https://www.youtube.com/watch?v=1tVCwv_BX2M
+//https://www.youtube.com/watch?v=Pb-8DzAObmg

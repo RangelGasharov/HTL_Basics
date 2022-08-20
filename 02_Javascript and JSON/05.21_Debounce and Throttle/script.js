1d01d0587403be68074483001d9b91e6084b6040
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-08-20 09:17:05 +0200

    Debounce and Throttle #1

---
+++ 02_Javascript and JSON/05.21_Debounce and Throttle/script.js
@@ -0,0 +1,64 @@
+const input = document.getElementById("input")
+const defaultText = document.getElementById("default")
+const debounceText = document.getElementById("debounce")
+const throttleText = document.getElementById("throttle")
+
+const updateDebounceText = debounce(() => {
+    incrementCount(debounceText)
+})
+
+const updateThrottleText = throttle(() => {
+    incrementCount(throttleText)
+}, 100)
+
+/*input.addEventListener("input", e => {
+    defaultText.textContent = e.target.value
+    updateDebounceText(e.target.value)
+    updateThrottleText(e.target.value)
+})*/
+
+function debounce(cb, delay = 1000) {
+    let timeout
+    return (...args) => {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+            cb(...args)
+        }, delay)
+    }
+}
+
+function throttle(cb, delay = 1000) {
+    let shouldWait = false
+    let waitingArgs
+    const timeoutFunc = () => {
+        if (waitingArgs == null) {
+            shouldWait = false
+        } else {
+            cb(...waitingArgs)
+            waitingArgs = null
+            setTimeout(timeoutFunc, delay)
+        }
+    }
+
+    return (...args) => {
+        if (shouldWait) {
+            waitingArgs = args
+            return
+        }
+
+        cb(...args)
+        shouldWait = true
+
+        setTimeout(timeoutFunc, delay)
+    }
+}
+
+document.addEventListener("mousemove", e => {
+    incrementCount(defaultText)
+    updateDebounceText()
+    updateThrottleText()
+})
+
+function incrementCount(element) {
+    element.textContent = (parseInt(element.innerText) || 0) + 1
+}

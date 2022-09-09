00c05e0ab1bd1895069086dbd9e33c941987768f
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-09-09 14:06:12 +0200

    Animated Website #2

--- 01_HTML and CSS/28_Animated Website/script.js
+++ 01_HTML and CSS/28_Animated Website/script.js
@@ -3,6 +3,6 @@ let moon = document.getElementById("moon")
 window.addEventListener("scroll", () => {
     scrollValue = window.scrollY;
     moon.style.marginLeft = `${scrollValue * 3}px`;
-    moon.style.marginBottom = `${scrollValue * -0.75}px`;
+    moon.style.marginBottom = `${scrollValue * -1.25}px`;
 })
 

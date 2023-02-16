22f097798cad3d1d7c553e62e1a634ea5de1b081
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-02-16 21:15:09 +0100

    Fetch API #1 - added examples of the Fetch API

---
+++ 02_Javascript and JSON/05.26_Fetch API/script.js
@@ -0,0 +1,13 @@
+fetch("https://reqres.in/api/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        name: "User 1"
+    })
+}).then(res => {
+    return res.json()
+})
+    .then(data => console.log(data))
+    .catch(error => console.log("Fetch was not successful!"))

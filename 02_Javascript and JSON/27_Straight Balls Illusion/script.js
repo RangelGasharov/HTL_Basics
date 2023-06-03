6c44740179528215e7d37df0d8b3af45a06f5d8d
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-06-03 22:59:53 +0200

    Straight Balls Illusion #3 - added buttons to toggle circles visibily, interesting result at the intermediate stand

--- 02_Javascript and JSON/27_Straight Balls Illusion/script.js
+++ 02_Javascript and JSON/27_Straight Balls Illusion/script.js
@@ -1,13 +1,10 @@
-let canvas = document.getElementById("canvas")
-let context = canvas.getContext("2d")
-
-let canvasWidth = canvas.width
-let canvasHeight = canvas.height
+const canvas = document.getElementById("canvas")
+const context = canvas.getContext("2d")
 let circles = []
 let bigRadius = 0.8 * canvas.width / 2;
-let amountOfCircles = 16;
-let centerX = canvasWidth / 2;
-let centerY = canvasHeight / 2;
+let amountOfCircles = 20;
+let centerX = canvas.width / 2;
+let centerY = canvas.height / 2;
 
 class Circle {
     constructor(radius, color) {
@@ -17,12 +14,13 @@ class Circle {
         this.radius = radius
         this.color = color
         this.travelingAngle
-        this.displacementX;
-        this.displacementY;
-        this.isGoingForward;
+        this.displacementX
+        this.displacementY
+        this.isGoingForward
         this.timeShift
         this.isVisible
         this.isMoving = false;
+        this.progress
     }
 
     moveAfterDelay() {
@@ -32,39 +30,43 @@ class Circle {
     }
 
     draw() {
-        context.beginPath();
-        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         if (this.isVisible) {
+            context.beginPath();
+            context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
             context.fillStyle = this.color;
+            context.fill();
+            context.closePath();
         }
-        context.fill();
-        context.closePath();
     }
 
     update() {
         if (this.isMoving) {
+            this.progress = (Math.sqrt((this.x - canvas.width / 2) ** 2 + (this.y - canvas.height / 2) ** 2)) / (2 * bigRadius)
+            let speed = 2 * Math.abs(Math.sin(0.5 * Math.PI * this.progress))
+
             if (Math.sqrt((this.x - canvas.width / 2) ** 2 + (this.y - canvas.height / 2) ** 2) > bigRadius) {
                 this.isGoingForward = !this.isGoingForward;
             }
 
             if (this.isGoingForward) {
-                this.x -= Math.cos(this.travelingAngle)
-                this.y -= Math.sin(this.travelingAngle)
+                this.x -= Math.cos(this.travelingAngle) * speed
+                this.y -= Math.sin(this.travelingAngle) * speed
             } else {
-                this.x += Math.cos(this.travelingAngle)
-                this.y += Math.sin(this.travelingAngle)
+                this.x += Math.cos(this.travelingAngle) * speed
+                this.y += Math.sin(this.travelingAngle) * speed
             }
+            console.log(circles[0].progress)
         }
         this.draw()
     }
 }
-
 
 function createButtons() {
     let buttonContainer = document.getElementById("buttons-container")
     for (i = 1; i <= amountOfCircles; i++) {
         let currentButton = document.createElement("button")
         currentButton.classList.add("btn-balls-toggle")
+        currentButton.value = i;
         currentButton.id = `btn-${i}`
         currentButton.textContent = `Ball ${i}`
         buttonContainer.append(currentButton)
@@ -76,7 +78,7 @@ function createCircles() {
         let circleRadius = 20;
         currentCircle = new Circle(circleRadius, "rgb(255, 150, 20)");
         currentCircle.id = i;
-        currentCircle.timeShift = i * 835;
+        currentCircle.timeShift = i * 1200;
         currentCircle.isVisible = true;
         currentCircle.travelingAngle = 2 * Math.PI / amountOfCircles * (i - 1);
         currentCircle.displacementX = Math.cos(currentCircle.travelingAngle) * bigRadius;
@@ -97,11 +99,22 @@ function animate() {
     });
 }
 
-
+function toggleCircle(circleNumber) {
+    let circle = circles[circleNumber - 1]
+    circle.isVisible = !circle.isVisible
+}
 
 animate()
-createButtons()
 createCircles()
+createButtons()
+
+const buttonsOfCircles = document.querySelectorAll(".btn-balls-toggle")
+buttonsOfCircles.forEach(button => {
+    button.addEventListener("click", () => {
+        let buttonValue = button.value
+        toggleCircle(buttonValue)
+    })
+})
 
 /* 20.05.2023, 16:00 - 18:15 */
 /* 20.05.2023, 19:00 - 19:15 */

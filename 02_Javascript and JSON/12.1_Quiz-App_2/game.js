1b0c73207ef6b9daa515c2107da9b24f37597bb8
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-08-04 21:11:06 +0200

    Quiz App #3 - added end- and  homepage, finished project

--- 02_Javascript and JSON/12.1_Quiz-App_2/game.js
+++ 02_Javascript and JSON/12.1_Quiz-App_2/game.js
@@ -59,7 +59,7 @@ function getNewQuestion() {
     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", score)
 
-        return window.location.assign("/end.html")
+        return window.location.assign("./end.html")
     }
 
     questionCounter++

c64df116927bfa47dc7155510a8cb29e9202a3c7
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-08-13 16:24:55 +0200

    Weather App #4 - added a javascript file, changed the html and css, finished project

--- 02_Javascript and JSON/30_Weather App/script.js
+++ 02_Javascript and JSON/30_Weather App/script.js
@@ -1,31 +1,37 @@
-window.addEventListener("load", () => {
-    let lon;
-    let lat;
-    let apiKey = "28c2aab6489ede3ef4c5ecc8467fc278";
-    let api;
+import { api_open_weather } from "../../APIs.js";
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-        requestApi()
+let weather = {
+    apiKey: api_open_weather,
+    fetchWeather: function (city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
+            .then((response) => response.json()).then((data) => this.displayWeather(data))
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = `Weather in ${name}`;
+        document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = `${temp}°C`;
+        document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+        document.querySelector(".wind").innerText = `Wind speed: ${speed} km/h`;
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
+}
 
-    function onSuccess(position) {
-        lon = position.coords.longitude;
-        lat = position.coords.latitude;
-    }
-
-    function onError(error) {
-        alert("Permission got denied")
-    }
-
-    function requestApi() {
-        api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
-        console.log(api)
-        fetch(api).then(response => response.json()).then(result => weatherDetails(result))
-    }
-
-    function weatherDetails(info) {
-        console.log(info);
-    }
-
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
 });
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search()
+    }
+})
+
+weather.fetchWeather("Bregenz")

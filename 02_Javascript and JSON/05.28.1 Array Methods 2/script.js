89f33aeb33e7e71511e3cde70f18ed7c31d4c17e
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2023-12-28 21:50:11 +0100

    Added more array methods

--- 02_Javascript and JSON/05.28.1 Array Methods 2/script.js
+++ 02_Javascript and JSON/05.28.1 Array Methods 2/script.js
@@ -32,3 +32,40 @@ console.log(f, people);
 console.log("%cFill", "font-size: 2em;");
 const g = e.fill(1, 1, 3);
 console.log(g, e);
+
+console.log("%cToSorted", "font-size: 2em;");
+const h = people.toSorted();
+console.log(h);
+
+console.log("%cToReversed", "font-size: 2em;");
+const i = people.toReversed();
+console.log(i);
+
+console.log("%cToSpliced", "font-size: 2em;");
+const j = people.toSpliced();
+console.log(j);
+
+console.log("%cFlat", "font-size: 2em;");
+let numbers = [
+    [1, 2], [3, 4], [[5, 6], [7, 8]]
+]
+console.log("%cInitial Array", "font-size: 1.5em;");
+console.log(numbers);
+
+const k = numbers.flat(Number.POSITIVE_INFINITY);
+console.log(k);
+
+numbers = [1, 2, 3, 0]
+console.log("%cInitial Array", "font-size: 1.5em;");
+console.log(numbers);
+
+console.log("%cFind Last", "font-size: 2em;");
+const l = numbers.findLast(n => n < 2);
+console.log(l);
+
+console.log("%cReduce Right", "font-size: 2em;");
+const m = numbers.reduceRight(n => n < 2);
+console.log(m);
+
+console.log("%cIs Array", "font-size: 2em;");
+console.log(Array.isArray(numbers));

ca0565e58fc23bcb94be9d4f77191caac821f318
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-01-18 23:18:04 +0100

    Added Tile class

--- 03_Games/01.8_2048 Game/Grid.js
+++ 03_Games/01.8_2048 Game/Grid.js
@@ -4,11 +4,48 @@ const CELL_GAP = 2
 
 
 export default class Grid {
+    #cells
     constructor(gridElement) {
+
         gridElement.style.setProperty("--grid-size", GRID_SIZE)
         gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`)
         gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`)
-        createCellElements(gridElement);
+        this.#cells = createCellElements(gridElement).map((cellElement, index) => {
+            return new Cell(cellElement, index % GRID_SIZE, Math.floor(index / GRID_SIZE))
+        })
+    }
+
+    get #emptyCells() {
+        return this.#cells.filter(cell => cell.tile == null);
+    }
+
+    randomEmptyCell() {
+        const randomIndex = Math.floor(Math.random() * this.#emptyCells.length);
+        return this.#emptyCells[randomIndex];
+    }
+}
+
+class Cell {
+    #cellElement
+    #x
+    #y
+    #tile
+
+    constructor(cellElement, x, y) {
+        this.#cellElement = cellElement;
+        this.#x = x;
+        this.#y = y;
+    }
+
+    get tile() {
+        return this.#tile;
+    }
+
+    set tile(value) {
+        this.#tile = value;
+        if (value == null) { return };
+        this.#tile.x = this.#x;
+        this.#tile.y = this.#y;
     }
 }
 
@@ -22,4 +59,3 @@ function createCellElements(gridElement) {
     }
     return cells
 }
-

52ce1cb1e5e6f010343a8da3b80dac2d4e551e44
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-01-19 23:25:50 +0100

    Begun with the Input Handling

--- 03_Games/01.8_2048 Game/Grid.js
+++ 03_Games/01.8_2048 Game/Grid.js
@@ -15,6 +15,14 @@ export default class Grid {
         })
     }
 
+    get cellsByColumn() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.x] = cellGrid[cell.x] || [];
+            cellGrid[cell.x][cell.y] = cell;
+            return cellGrid;
+        }, [])
+    }
+
     get #emptyCells() {
         return this.#cells.filter(cell => cell.tile == null);
     }
@@ -35,6 +43,14 @@ class Cell {
         this.#cellElement = cellElement;
         this.#x = x;
         this.#y = y;
+    }
+
+    get x() {
+        return this.#x;
+    }
+
+    get y() {
+        return this.#y;
     }
 
     get tile() {
--- 03_Games/01.8_2048 Game/script.js
+++ 03_Games/01.8_2048 Game/script.js
@@ -7,3 +7,31 @@ const grid = new Grid(gameBoard);
 
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
+
+function setupInput() {
+    window.addEventListener("keydown", handleInput, { once: true });
+}
+
+function handleInput(e) {
+    switch (e.key) {
+        case "ArrowUp":
+            moveUp()
+            break
+        case "ArrowDown":
+            moveDown()
+            break
+        case "ArrowLeft":
+            moveLeft()
+            break
+        case "ArrowRight":
+            moveRight()
+            break
+        default:
+            setupInput()
+            return
+    }
+}
+
+function moveUp() {
+
+}

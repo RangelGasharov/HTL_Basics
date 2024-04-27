69925302ae130e91d1ca058c2cba22ab9a2c4308
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-04-27 18:24:39 +0200

    Added a function that returns the name of a generation by the number of generation x and the gender y

--- 02_Javascript and JSON/Coding Challenges/Number of Boomerangs/script.js
+++ 02_Javascript and JSON/Coding Challenges/Number of Boomerangs/script.js
@@ -12,14 +12,33 @@ function amountOfBoomerangs(array) {
     return amountOfBoomerangs;
 }
 
+console.log(amountOfBoomerangs([1, 3, 1, 3, 2, 1, -8, 1, -8, 6, -5, 6]))
+
 function possibleBonus(a, b) {
     if (a >= b) { return false; }
     if (b - a > 6) { return false; }
     return true;
 }
 
-console.log(amountOfBoomerangs([1, 3, 1, 3, 2, 1, -8, 1, -8, 6, -5, 6]))
-
 console.log(possibleBonus(4, 10));
 console.log(possibleBonus(1, 10));
 console.log(possibleBonus(10, 10));
+
+
+function getGeneration(x, y) {
+    const generations = {
+        "-3": { "m": "great grandfather", "f": "great grandmother" },
+        "-2": { "m": "grandfather", "f": "grandmother" },
+        "-1": { "m": "father", "f": "mother" },
+        "0": { "m": "me!", "f": "me!" },
+        "1": { "m": "son", "f": "daughter" },
+        "2": { "m": "grandson", "f": "granddaughter" },
+        "3": { "m": "great grandson", "f": "great granddaughter" }
+    };
+    return generations[x][y];
+}
+
+console.log(getGeneration(-3, "m"))
+console.log(getGeneration(-2, "f"))
+console.log(getGeneration(1, "f"))
+console.log(getGeneration(0, "m"))
